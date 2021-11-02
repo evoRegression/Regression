a4783d1d9d61950ca61a12b90df41b17f862fe85
zsolt2 <79489624+zsolt2@users.noreply.github.com>
2021-11-02 20:48:55 +0100

    MLContext, MLModel

---
+++ OOPExercise/MLContext/MLContext.cs
@@ -0,0 +1,30 @@
+﻿using DataProvider;
+using MLModel;
+
+namespace MLContext 
+{
+    public class MLContext : IMLContext
+    {
+        private IDataProvider dataProvider;
+        private IMLModel model;
+        private int[] data;
+
+        public void Init(IDataProvider dataProvider, IMLModel model)
+        {
+            this.dataProvider = dataProvider;
+            this.model = model;
+            this.data = this.dataProvider.Read("Placeholder text");
+
+        }
+
+        public int Predict(int dataPoint)
+        {
+            return this.model.Predict(dataPoint);
+        }
+
+        public void Train()
+        {
+            this.model.Train(this.data);
+        }
+    }
+}
---
+++ OOPExercise/MLModel/MLModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLModel
+{
+    public class MLModel : IMLModel
+    {
+        public MLModel()
+        {
+            this.trainedData = null;
+        }
+        public double Evaluation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Predict(int dataPoint)
+        {
+            if (trainedData == null)
+            {
+                throw new Exception("Modell is not trained");
+            }
+            if (dataPoint < trainedData)
+                return dataPoint;
+            return (int)trainedData;
+        }
+        public void Train(int[] data)
+        { 
+            if (data == null || data.Length == 0)
+                throw new Exception("Data array is empty");
+            int min = int.MaxValue;
+            foreach( int i in data)
+            {
+                if (min > i)
+                    min = i;
+            }
+            trainedData = min;
+        }
+        private int? trainedData;
+    }
+}
