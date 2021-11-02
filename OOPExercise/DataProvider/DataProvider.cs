975cf1be31bcfe42c863e902a54d4814110a4f15
zsolt2 <79489624+zsolt2@users.noreply.github.com>
2021-11-02 18:46:01 +0100

    Data Provider

---
+++ OOPExercise/DataProvider/DataProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider
+{
+    class DataProvider : IDataProvider
+    {
+        private int[] data;
+        public double Mean()
+        {
+            if (this.data == null || this.data.Length == 0)
+                throw new Exception("Data array is empty");
+
+            return (double)this.data.Sum() / this.data.Length;
+        }
+
+        public double Median()
+        {
+            if (this.data == null || this.data.Length == 0)
+                throw new Exception("Data array is empty");
+            int[] sortedData = (int[])this.data.Clone();
+            int midIndex = sortedData.Length / 2 + 1;
+            if( sortedData.Length % 2 == 0)
+            {
+                return (sortedData[midIndex] + sortedData[midIndex - 1]) / 2.0;
+            }
+            else
+            {
+                return sortedData[midIndex];
+            }
+        }
+
+        public int[] Read(string filePath)
+        {
+            this.data = new int[] { 1, 34, 634, 674, 34, 56, 23, 4 };
+            return data;
+        }
+
+        public double StandardDeviation()
+        {
+            return Math.Sqrt(this.Variance());
+        }
+
+        public double Variance()
+        {
+            if (this.data == null || this.data.Length == 0)
+                throw new Exception("Data array is empty");
+            double mean = this.Mean();
+            double sum = 0;
+            foreach (int i in this.data)
+            {
+                sum += Math.Pow(i - mean, 2.0);
+            }
+            return sum / this.data.Length;
+        }
+    }
+}
