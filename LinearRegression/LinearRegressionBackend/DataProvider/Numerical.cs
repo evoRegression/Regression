a5a81f3e8d57cfe8d9ce63b75166e992a1eaf503
cnsor <cnsorbeehrt@gmail.com>
2021-12-16 14:59:59 +0100

    wip

--- LinearRegression/LinearRegressionBackend/DataProvider/Numerical.cs
+++ LinearRegression/LinearRegressionBackend/DataProvider/Numerical.cs
@@ -14,10 +14,17 @@ namespace LinearRegressionBackend.DataProvider
         /// <param name="stop">Exclusive stoping value.</param>
         /// <param name="step">Number of steps go from <paramref name="start"/> to <paramref name="stop"/>.</param>
         /// <returns>Returns with an array.</returns>
+
         public static double[] Arange(double start, double stop, double step)
         {
-            double[] array = null;
-
+            double[] array = new double [(int)((stop-start)/step)];
+            for (double i=start;i<stop;i+=step)
+            {
+                for (int j = 0;; j++)
+                {
+                    array[j] = i;
+                }
+            }
             return array;
         }
 
@@ -51,9 +58,40 @@ namespace LinearRegressionBackend.DataProvider
         /// <param name="axis"></param>
         public static double Mean(double[][] matrix, int axis)
         {
-            double result = 0;
+            int[] matrix_array;
+            if (axis == 0)
+            {
+                for (int i = 0; i < matrix.GetLength(0) / 2; i++)
+                {
 
-            return result;
+                }
+            }
+            return 0;
+            /*
+            int middle = matrix.GetLength(0)/2;
+            if (middle % 2 == 0)
+            {
+                if (axis == 0)
+                {
+                    return ( (matrix[middle][0]+ matrix[middle+1][0]) / 2 );
+                }
+                else
+                {
+                    return ((matrix[middle][0] + matrix[middle + 1][0]) / 2);
+                }
+            }
+            else
+            {
+                
+                if (axis == 0)
+                {
+                    return matrix[middle][0];
+                }
+                else
+                {
+                    return matrix[middle][1];
+                }
+            }*/
         }
 
         /// <summary>
@@ -114,6 +152,10 @@ namespace LinearRegressionBackend.DataProvider
             double result = 0;
 
             return result;
+        }
+        public static double[] SortArray(double[] array)
+        {
+            return SortArray(array);
         }
     }
 }
