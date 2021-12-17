1dd43cb2415796644bb1c056e1ef79e575146492
Molnár Balázs <molbazsa@gmail.com>
2021-12-17 11:16:58 +0100

    Draw line from m and b

--- LinearRegression/LinearRegressionWPF/ViewModels/MainWindowViewModel.cs
+++ LinearRegression/LinearRegressionWPF/ViewModels/MainWindowViewModel.cs
@@ -13,11 +13,48 @@ namespace LinearRegressionWPF.ViewModels
         public ICommand OpenDataFileCommand { get; private set; }
         public ICommand TrainCommand { get; private set; }
 
+        private double _slope;
+        private double _yIntercept;
+
         public MainWindowViewModel()
         {
             RegressionPlot = new RegressionPlot();
             OpenDataFileCommand = new OpenDataFile(this);
             TrainCommand = new Train(this);
+        }
+
+        public double Slope
+        {
+            get
+            {
+                return _slope;
+            }
+
+            set
+            {
+                _slope = value;
+                updateRegressionLine(_slope, _yIntercept);
+            }
+        }
+
+        public double YIntercept
+        {
+            get
+            {
+                return _yIntercept;
+            }
+
+            set
+            {
+                _yIntercept = value;
+                updateRegressionLine(_slope, _yIntercept);
+            }
+        }
+
+        private void updateRegressionLine(double slope, double yIntercept)
+        {
+            RegressionLine line = new RegressionLine(slope, yIntercept, 0, 10);
+            RegressionPlot.updateRegressionLine(line);
         }
     }
 }
