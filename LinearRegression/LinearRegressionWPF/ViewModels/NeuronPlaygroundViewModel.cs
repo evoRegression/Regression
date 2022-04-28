797f6e3a96ac5c89e98d3bccb1cc0f9641b50d33
Molnár Balázs <molbazsa@gmail.com>
2022-04-28 18:21:53 +0200

    Neuron playground skeleton

---
+++ LinearRegression/LinearRegressionWPF/ViewModels/NeuronPlaygroundViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+using LinearRegressionBackend.MLNeuralNetwork;
+
+using LinearRegressionWPF.Commands;
+
+namespace LinearRegressionWPF.ViewModels
+{
+    internal class NeuronPlaygroundViewModel : INotifyPropertyChanged
+    {
+        public NeuronPlaygroundViewModel()
+        {
+            InitWeights();
+            InitActivation();
+            InitCommands();
+        }
+
+        public void InitCommands()
+        {
+            // pass
+        }
+
+        #region Weights
+
+        public void InitWeights()
+        {
+            // pass
+        }
+
+        #endregion
+
+        #region Activation
+
+        public void InitActivation()
+        {
+            // pass
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
