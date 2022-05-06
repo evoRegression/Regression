6084a681dd4a66bd9bf58fa54790ddf4920c661e
Ali Jlidi <37947410+aliJlidi@users.noreply.github.com>
2022-05-06 14:11:51 +0200

    adding

---
+++ LinearRegression/LinearRegressionBackend/DataProvider/Numerical.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace LinearRegressionBackend.DataProvider
+{
+    /// <summary>
+    /// Helper class for calculating numerical operations on matrixes.
+    /// </summary>
+    public static class Numerical
+    {
+        /// <summary>
+        /// Generates a vector from <paramref name="start"/> inclusively to <paramref name="stop"/> exclusively.
+        /// </summary>
+        /// <param name="start">Inclusive starting value.</param>
+        /// <param name="stop">Exclusive stoping value.</param>
+        /// <param name="step">Number of steps go from <paramref name="start"/> to <paramref name="stop"/>.</param>
+        /// <returns>Returns with an array.</returns>
+        public static double[] Arange(double start, double stop, double step)
+        {
+            int j = 0;
+            double[] array = new double [(int)((stop-start)/step)];
+            for (double i=start;i<stop;i+=step)
+            {
+                array[j++]=i;
+            }
+            return array;
+
+            //linq enumarate-range
+        }
+
+        /// <summary>
+        /// Loads a correctly formated string file, into a two dimensional double matrix. The format must be "number,number", 
+        /// every line can only contain 1 pair of values, the file must contain at least one full pair.
+        /// </summary>
+        /// <param name="filePath">The path to the formated file, from which we want to get the values</param>
+        /// <param name="delimeter">The char symbol, we used to sepparate the number pairings during the formating process.</param>
+        /// <returns>Returns with a two dimensional double matrix.</returns>
+        public static double[][] LoadText(string filePath, char delimeter = ',')
+        {
+            double[][] matrix = null;
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+            List<double[]> dataList = new List<double[]>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                String line = lines[i];
+                String[] numbers = line.Split(delimeter);
+                if (numbers.Length != 2)
+                {
+                    throw new InvalidDataException($"Incorrect Data in \"{ filePath}\" file at line: {i + 1} : {line}");
+                }
+                double[] XandY = new double[2];
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    double nextData;
+                    if (double.TryParse(numbers[j], NumberStyles.Any, CultureInfo.InvariantCulture, out nextData))
+                        XandY[j] = nextData;
+                    else
+                        throw new InvalidDataException($"Incorrect Data in \"{ filePath}\" file at line: {i + 1} : {line}");
+                }
+                dataList.Add(XandY);
+            }
+            matrix = dataList.ToArray();
+            return matrix;
+        }
+
+        /// <summary>
+        /// Saves, and formats a two dimensional double matrix, into a txt file.
+        /// </summary>
+        /// <param name="filePath">The path to the txt file we want to save our matrix to.</param>
+        /// <param name="matrix">The matrix we wish to save</param>
+        /// <param name="delimeter">The symbol in the txt that will be used to separate the value pairs.</param>
+        public static void SaveText(string filePath, double[][] matrix, char delimeter = ',')
+        {
+            string outputstring= matrix[0][0].ToString() + delimeter.ToString() + matrix[0][1].ToString() + Environment.NewLine;
+            for (int i = 1; i < matrix.GetLength(0); i++)
+            {
+                outputstring += matrix[i][0].ToString() + delimeter.ToString() + matrix[i][1].ToString() + Environment.NewLine;
+            }
+            File.WriteAllText(filePath, outputstring);
+        }
+
+        /// <summary>
+        /// Calculates the mean of all values on an axis.
+        /// </summary>
+        /// <param name="matrix">The matrix, containing the values.</param>
+        /// <param name="axis">The axis whose mean we want to calculate. 0 for X, 1 for Y.</param>
+        /// <returns>Returns the mean of all values, of the given axis.</returns>
+        public static double Mean(double[][] matrix, int axis)
+        {
+         double result = matrix[0][axis];
+             for (int i = 1; i < matrix.GetLength(0); i++)
+             {
+                 result += matrix[i][axis];
+             }
+         return result/ matrix.GetLength(0);            
+        }
+
+        /// <summary>
+        /// Calculates the Median of all values on an axis.
+        /// </summary>
+        /// <param name="matrix">The matrix, containing the values.</param>
+        /// <param name="axis">The axis whose median we want to calculate. 0 for X, 1 for Y.</param>
+        /// <returns>Returns the Median of all values, from the given axis.</returns>
+        public static double Median(double[][] matrix, int axis)
+        {
+            double[] matrix_array = new double[matrix.GetLength(0)];
+            double middle = matrix_array.GetLength(0) / 2;
+            if (axis == 0)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    matrix_array[i] = matrix[i][axis];
+                }
+                SortArray(matrix_array);
+                if (matrix_array.Length % 2 == 0)
+                {
+                    double egy = matrix_array[(int)middle - 1];
+                    double ketto = matrix_array[(int)middle];
+                    return ((matrix_array[(int)middle - 1] + matrix_array[(int)middle]) / 2);
+                }
+                else
+                {
+                    return (matrix_array[(int)middle]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    matrix_array[i] = matrix[i][axis];
+                }
+                SortArray(matrix_array);
+                if (matrix_array.Length % 2 == 0)
+                {
+                    double egy = matrix_array[(int)middle - 1];
+                    double ketto = matrix_array[(int)middle];
+                    return ((matrix_array[(int)middle - 1] + matrix_array[(int)middle]) / 2);
+                }
+                else
+                {
+                    return (matrix_array[(int)middle]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Calculates the standard deviation of all values on an axis.
+        /// </summary>
+        /// <param name="matrix">The matrix, containing the values.</param>
+        /// <param name="axis">The axis whose standard deviation we want to calculate. 0 for X, 1 for Y.</param>
+        /// <returns>Returns the Standard deviation of all values, from the given axis.</returns>
+        public static double StandardDeviation(double[][] matrix, int axis)
+        {
+            double mean = Numerical.Mean(matrix,axis);
+            double result=0;
+            //double[] matrix_array = new double[matrix.GetLength(0)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                result += Math.Pow((matrix[i][axis]-mean),2);
+            }
+            result = result / (matrix.GetLength(0) - 1);
+            return Math.Pow(result,0.5);
+        }
+
+        /// <summary>
+        /// Calculates the variance of all values on an axis.
+        /// </summary>
+        /// <param name="matrix">The matrix, containing the values.</param>
+        /// <param name="axis">The axis whose variance we want to calculate. 0 for X, 1 for Y.</param>
+        /// <returns>Returns the variance of all values, from the given axis.</returns>
+        public static double Variance(double[][] matrix, int axis)
+        {
+            double result = Max(matrix,axis) - Min(matrix,axis);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Calculates the minimum value on an axis.
+        /// </summary>
+        /// <param name="matrix">The matrix, containing the values.</param>
+        /// <param name="axis">The axis whose minumim we want to calculate. 0 for X, 1 for Y.</param>
+        /// <returns>Returns the lowest value, from the given axis.</returns>
+        public static double Min(double[][] matrix, int axis)
+        {
+            double result = matrix[0][axis];
+            for (int i = 1; i < matrix.GetLength(0); i++)
+            {
+                if (matrix[i][axis] < result)
+                {
+                    result = matrix[i][axis];
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Calculates the maximum value on an axis.
+        /// </summary>
+        /// <param name="matrix">The matrix, containing the values.</param>
+        /// <param name="axis">The axis whose maximum we want to calculate. 0 for X, 1 for Y.</param>
+        /// <returns>Returns the largest value, from the given axis.</returns>
+        public static double Max(double[][] matrix, int axis)
+        {
+            double result = matrix[0][axis];
+            for (int i = 1; i < matrix.GetLength(0); i++)
+            {
+                if(matrix[i][axis]>result)
+                {
+                    result = matrix[i][axis];
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Sorts an array, than returns it shorted
+        /// </summary>
+        /// <param name="array">The double array, we want to short.</param>
+        /// <returns>Returns the array value, shorted.</returns>
+        public static double[] SortArray(double[] array)
+        {
+            double[] returnArray= array;
+            Array.Sort(returnArray);
+            return returnArray;
+        }
+    }
+}
---
+++ LinearRegression/LinearRegressionBackend/MLModel/LinearRegressionModel.cs
@@ -0,0 +1,105 @@
+﻿using LinearRegressionBackend.MLCommmons;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace LinearRegressionBackend.MLModel
+{
+
+    public class LinearRegressionModel : IMLModel
+    {
+        internal Coefficients _coefficient;
+        internal IOptimizer _optimizer;
+        internal ILossFunction _lossFunction;
+
+        public LinearRegressionModel( IOptimizer optimizer, ILossFunction lossFunction)
+        {
+            _coefficient = new Coefficients(0, 0);
+            _optimizer = optimizer;
+            _lossFunction = lossFunction;
+        }
+
+        public LinearRegressionModel(double Slope, double Intercept, IOptimizer optimizer, ILossFunction lossFunction)
+        {
+            _coefficient = new Coefficients(Slope, Intercept);
+            _optimizer = optimizer;
+            _lossFunction = lossFunction;
+        }
+
+        public double Evaluation(double[][] inputData, double[] targetData)
+        {
+            return _lossFunction.Loss(_coefficient.getThetas(), inputData, targetData);
+        }
+
+        public List<History> Fit(double[][] inputData, double[] targetData, int epochs = 1)
+        {
+            List<History> history= new List<History>();
+
+            if (_optimizer is IIterable == false )
+            {
+                epochs = 1;
+                _coefficient.Slope = 0;
+                _coefficient.Intercept = 0;
+            }
+            
+            for (int i = 0; i < epochs; i++)
+            {
+                double[] newThetas = _optimizer.Minimize(_lossFunction, _coefficient.getThetas(), inputData, targetData);
+                _coefficient.Slope += newThetas[MLCommons.SLOPE_INDEX];
+                _coefficient.Intercept += newThetas[MLCommons.INTERCEPT_INDEX];
+                history.Add(new History(_lossFunction.Loss(_coefficient.getThetas(), inputData, targetData), _coefficient.getThetas()));
+            }
+            return history;
+        }
+
+        public double Predict(double[] inputData)
+        {
+            return _coefficient.Slope * inputData[0] + _coefficient.Intercept;
+        }
+
+        public void Save(string path)
+        {
+            using (FileStream fs = new(path, FileMode.Create))
+            {
+                using (XmlDictionaryWriter writer = XmlDictionaryWriter.CreateTextWriter(fs))
+                {
+                    DataContractSerializer dcs = new DataContractSerializer(typeof(Coefficients));
+                    dcs.WriteObject(writer, _coefficient);
+                }
+            }
+        }
+
+        public IMLModel Load(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                XmlDictionaryReader reader = XmlDictionaryReader.CreateTextReader(fs, new XmlDictionaryReaderQuotas());
+                DataContractSerializer dcs = new DataContractSerializer(typeof(Coefficients));
+
+                Coefficients coefficient = (Coefficients)dcs.ReadObject(reader);
+
+                return new LinearRegressionModel(coefficient.Slope, coefficient.Intercept, null, null);
+            }
+        }
+
+        public void setLearningRate(double learningRate)
+        {
+            if (_optimizer is IIterable)
+            {
+                ((IIterable)_optimizer).learningRate = learningRate;
+            }
+        }
+
+        public double getLearningRate()
+        {
+            if (_optimizer is IIterable)
+            {
+                return ((IIterable)_optimizer).learningRate;
+            }
+            return 0;
+        }
+    }
+}
