08a0f2f2a6bb2041fa1723cf50d5052e8b5be74b
Molnár Balázs <molbazsa@gmail.com>
2022-06-01 09:41:09 +0200

    Refactor Sigmoid derivative

--- LinearRegression/LinearRegressionBackend/MLNeuralNetwork/Sigmoid.cs
+++ LinearRegression/LinearRegressionBackend/MLNeuralNetwork/Sigmoid.cs
@@ -11,8 +11,8 @@ namespace LinearRegressionBackend.MLNeuralNetwork
 
         public double Derivative(double weightedSum)
         {
-            return Math.Pow(Math.E, -weightedSum)
-                / Math.Pow(1.0 + Math.Pow(Math.E, -weightedSum), 2);
+            double s = Activation(weightedSum);
+            return s * (1 - s);
         }
     }
 }
