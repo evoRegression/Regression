e0752f2109e89bb4b5df20d4df40a1e4470eb48a
Peter Szilvasi <peti.szilvasi95@gmail.com>
2022-07-04 11:54:22 +0200

    Adds random shuffling for the two shapes scenario
    First argument specify the number of random samples to be retrieved.
    Remarks: We need to extract the folder manually, because there is no clean solution without using NuGet packages.

---
+++ LinearRegression/RandomShuffle/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RandomShuffle
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            string resourcePath = Path.GetFullPath(@"..\..\..\..\Resources");
+
+            string extractionPath = Path.Combine(resourcePath, "Output");
+            string randomSampleFolder = Path.Combine(resourcePath, "Random");
+
+            CreateRandomSampleFolder(randomSampleFolder);
+
+            List<string> listOfFiles = Directory.GetFiles(extractionPath).ToList();
+
+            int numberOfSample = int.Parse(args[0]);
+            for (int i = 0; i < numberOfSample; i++)
+            {
+                string randomFile = listOfFiles.PopRandom();
+                FileInfo file = new FileInfo(randomFile);
+
+                File.Copy(file.FullName, Path.Combine(randomSampleFolder, $"{i}.png"));
+            }
+        }
+
+        private static void CreateRandomSampleFolder(string extractPath)
+        {
+            if (Directory.Exists(extractPath))
+            {
+                Directory.Delete(extractPath, true);
+            }
+
+            Directory.CreateDirectory(extractPath);
+        }
+    }
+}
